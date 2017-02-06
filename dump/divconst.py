1685f6bfccdcf69bfc94cb711ce9b86b2e646c2f
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-06 11:06:30 +0100

    division - iterate over precisions to get best match

--- dump/divconst.py
+++ dump/divconst.py
@@ -27,52 +27,55 @@ parser.add_argument('--align', dest='align', default=False, action='store_const'
                     help='Align shift to 8bits')
 args = parser.parse_args()
 
-
-R = mpmath.mpmathify(long(args.num, 16))
-mp.dps = args.dps
-Rlog = math.log(R, 2)
-Rlog_exact2 = mpmath.log(R, 2)
-logger.info('R: %s, log: %s, exactmp: %s' % (R, Rlog, Rlog_exact2))
-
-
 align_8 = args.align
-best_i = None
-best_a = None
-min_dif = None
-for i in range(1, args.top):
-    power = mpmath.power(2, i)
+R = mpmath.mpmathify(long(args.num, 16))
 
-    # 1/R = a / 2^(k)
-    #   R = 2^k / a
-    #   a = 2^k / R
-    if power < R:
-        continue
+for dps in range(1, args.dps, 5):
+    mp.dps = dps
+    Rlog = math.log(R, 2)
+    Rlog_exact2 = mpmath.log(R, 2)
+    logger.info('DPS: %d, R: %s, log: %s, \n\texactmp: %s' % (dps, R, Rlog, Rlog_exact2))
 
-    # http://mpmath.org/doc/current/general.html#fdiv
-    a = mpmath.nint(mpmath.fdiv(power, R, dps=150))
-    # a = long(round(power / float(R)))
+    best_i = None
+    best_a = None
+    min_dif = None
+    for i in range(1, args.top):
+        power = mpmath.power(2, i)
 
-    # R = 2^k / a
-    # log R = k - log a
-    # Rlogexact - (k - log(a))
-    Rdiflog = mpmath.fabs(Rlog_exact2 - (i - mpmath.log(a, 2)))
-    #logger.info('...a: %s, denominator: 2^%s, \ndiflog: %s %s' % (a, i, Rdiflog, 'X' if i%8 == 0 else ''))
+        # 1/R = a / 2^(k)
+        #   R = 2^k / a
+        #   a = 2^k / R
+        if power < R:
+            continue
 
-    if min_dif is None or min_dif > abs(Rdiflog) and (not align_8 or i % 8 == 0):
-        min_dif = Rdiflog
-        best_a = long(a)
-        best_i = i
+        # http://mpmath.org/doc/current/general.html#fdiv
+        a = mpmath.nint(mpmath.fdiv(power, R, dps=150))
+        # a = long(round(power / float(R)))
 
-# Precision test:
-R_prec = mpmath.fdiv(2**best_i, best_a) - R
+        # R = 2^k / a
+        # log R = k - log a
+        # Rlogexact - (k - log(a))
+        Rdiflog = mpmath.fabs(Rlog_exact2 - (i - mpmath.log(a, 2)))
+        #logger.info('...a: %s, denominator: 2^%s, \ndiflog: %s %s' % (a, i, Rdiflog, 'X' if i%8 == 0 else ''))
 
-# R * 1/R
-R_self = (long(R) * long(best_a)) >> best_i
-R_self2 = (long(R*2) * long(best_a)) >> best_i
-R_self3 = (long(R*3) * long(best_a)) >> best_i
-R_self4 = (long(R*4) * long(best_a)) >> best_i
+        if min_dif is None or min_dif > abs(Rdiflog) and (not align_8 or i % 8 == 0):
+            min_dif = Rdiflog
+            best_a = long(a)
+            best_i = i
 
-logger.info('Terminated, best diff: %s, \nbest a: %s, \nbest denominator: 2^%s, \nR-prec: %s\nR-self: %s, %s, %s, %s'
-            % (min_dif, best_a, best_i, R_prec, R_self, R_self2, R_self3, R_self4))
+    # Precision test:
+    R_prec = mpmath.fdiv(2**best_i, best_a) - R
+
+    # R * 1/R
+    R_self = (long(R) * long(best_a)) >> best_i
+    R_self2 = (long(R*2) * long(best_a)) >> best_i
+    R_self3 = (long(R*3) * long(best_a)) >> best_i
+    R_self4 = (long(R*4) * long(best_a)) >> best_i
+    R_self12345 = (long(R*12345) * long(best_a)) >> best_i
+    R_self65537 = (long(R*65537) * long(best_a)) >> best_i
+
+    logger.info('Terminated, best \n\tdiff: %s, \n\tbest a: %s, \n\tbest denominator: 2^%s, \n\tR-prec: %s'
+                '\n\tR-self: %s, %s, %s, %s, %s, %s'
+                % (min_dif, best_a, best_i, R_prec, R_self, R_self2, R_self3, R_self4, R_self12345, R_self65537))
 
 
